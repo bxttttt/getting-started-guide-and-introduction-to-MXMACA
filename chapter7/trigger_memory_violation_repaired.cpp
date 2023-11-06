ba1e484af185ac12a8765ebbdbec77ce8c91b713
bxttttt <2564757906@qq.com>
2023-11-06 09:42:39 +0800

    Create trigger_memory_violation_repaired.cpp

---
+++ chapter7/trigger_memory_violation_repaired.cpp
@@ -0,0 +1,26 @@
+#include<mc_runtime_api.h>
+
+typedef struct 
+{
+  float f;
+  double d;
+}test_type_mem_violation;
+
+__global__ void trigger_memory_violation(test_type_mem_violation *dst)
+{
+  atomicAdd(&dst->f,1.23);
+  atomicAdd(&dst->d,20);
+  dst->f=9.8765;
+}
+
+int main()
+{
+  test_type_mem_violation hd={0};
+  test_type_mem_violation *ddd;
+  mcMalloc((void**)&ddd,sizeof(test_type_mem_violation));
+  mcMemcpy(ddd,&hd,sizeof(test_type_mem_violation),mcMemcpyHostToDevice);
+  trigger_memory_violation<<<dim3(1),dim3(1)>>>(ddd);
+  mcMemcpy(&hd,ddd,sizeof(test_type_mem_violation),mcMemcpyDeviceToHost);
+  mcFree(ddd);
+  return 0;
+}
